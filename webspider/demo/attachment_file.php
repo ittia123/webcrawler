f00383af34bea93d38702496bfae1f469f023bef
Andy <lish000@126.com>
2016-12-24 00:19:02 +0800

    first commit

---
+++ webspider/demo/attachment_file.php
@@ -0,0 +1,31 @@
+<?php
+ini_set("memory_limit", "1024M");
+require dirname(__FILE__).'/../core/init.php';
+
+/* Do NOT delete this comment */
+/* 不要删除这段注释 */
+
+$spider = new phpspider();
+
+$spider->on_attachment_file = function($url, $filetype, $phpspider) 
+{
+    // 输出文件URL地址和文件类型
+    //var_dump($url, $filetype);
+
+    if ($filetype == 'jpg') 
+    {
+        // 以纳秒为单位生成随机数
+        $filename = uniqid();
+        // 在data目录下生成图片
+        $filepath = PATH_DATA."/{$filename}.jpg";
+        // 用系统自带的下载器wget下载
+        exec("wget {$url} -O {$filepath}");
+
+        // 用PHP函数下载，容易耗尽内存，慎用
+        //$data = file_get_contents($attachment_url);
+        //file_put_contents($filepath, $attachment_url);
+    }
+};
+
+$url = "https://movie.douban.com/";
+$spider->request_url($url);
